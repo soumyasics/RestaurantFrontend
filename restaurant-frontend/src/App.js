c9e164ed8e4a253a1937b975be6fc6df7253ccad
impulzz <sajinraj068@gmail.com>
2024-01-10 11:07:31 +0530

    sajincommit

--- restaurant-frontend/src/App.js
+++ restaurant-frontend/src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
+import Login from './Project/Login';
+import Register from './Project/Register';
+import Nav from './Project/Nav';
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <BrowserRouter>
+    <Nav/>
+    <Routes>
+    <Route exact path="/Login" element={<Login/>}/>
+    <Route exact path="/Register" element={<Register/>}/>
+    </Routes>
+    </BrowserRouter>
+
+ </div>
+ 
+  
   );
 }
 
