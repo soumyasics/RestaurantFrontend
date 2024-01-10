7dc0edd235223b0e9e8acb729db9e9a89f2ccb0a
Bennet R <Bennet1605>
2024-01-10 10:52:35 +0530

    Bennet first commit

--- restaurant-frontend/src/App.js
+++ restaurant-frontend/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Login from "./Project/Login";
+import Register from "./Project/Register";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Nav from "./Project/Nav";
+import Forgetpassword from "./Project/Forgetpassword";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Nav/>
+      <Routes>
+      <Route exact path="/Login" element={<Login/>}/>
+      <Route exact path="/Register" element={<Register/>}/>
+      <Route exact path="/Forgetpassword" element={<Forgetpassword/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
