2e835d8a9b146a88ab1ae9cd79c5c5e7b527a426
Bennet1605 <bennetregukumar@gmail.com>
2024-01-15 10:22:55 +0530

    Bennet

---
+++ restaurant-frontend/src/Project/Addfood.js
@@ -0,0 +1,77 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Addfood() {
+  const [data, setData] = useState({
+    foodname: "",
+    image: "",
+    price: "",
+    descripition: "",
+  });
+
+  const handleChange = (a) => {
+    setData({ ...data, [a.target.name]: a.target.value });
+  };
+
+  const handleSubmit = (a) => {
+    a.preventDefault();
+    console.log(data);
+    axios
+      .post("http://localhost:3500/addfood", data)
+      .then((res) => {
+        console.log(res);
+        if (res.data.status === 200) {
+          alert(res.data.msg);
+        } else {
+          alert(res.data.msg);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <div>
+      <form>
+        <div>Add food items</div>
+        <div>
+          <label>Food name:</label>
+          <input
+            type="text"
+            placeholder="Enter the food name"
+            name="foodname"
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <label>Food image:</label>
+          <input type="file" name="image" onChange={handleChange}></input>
+        </div>
+        <div>
+          <label>Price:</label>
+          <input
+            type="tel"
+            placeholder="Enter the price"
+            name="price"
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div>
+          <label>Descripition:</label>
+          <textarea
+            type="text"
+            placeholder="Enter the Descripition"
+            name="descripition"
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div>
+          <button onClick={handleSubmit}>Add item</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Addfood;
